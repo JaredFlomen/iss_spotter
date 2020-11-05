322580bff58096f4c528d00509d585f324d3e8ab
Jared <jaredflomen@gmail.com>
2020-11-05 22:20:57 +0000

    Retreiving location data from IP

--- iss.js
+++ iss.js
@@ -1,11 +1,3 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require('request');
 
 const fetchMyIP = function(callback) {
@@ -15,16 +7,34 @@ const fetchMyIP = function(callback) {
       callback(error, null);
       return;
     }
-    
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
-    
+    //RETREIVES THE IP
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
 };
 
-module.exports = fetchMyIP;
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const latitude = JSON.parse(body).lat;
+    const longitude = JSON.parse(body).lon;
+    callback({latitude, longitude}, null);
+  });
+};
+
+module.exports = {fetchMyIP, fetchCoordsByIP};
+
